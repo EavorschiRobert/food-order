82456ce5e82e905c710b990cf20ba36dea4f27c3
Eavorschi Robert Daniel <eavorschirobertdaniel@gmail.com>
2024-03-13 10:10:38 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 
-import Header from "./components/Header";
-import Meals from "./components/Meals";
-import CartContextProvider from "./context/CartContext";
-import Cart from "./components/Cart";
-
 function App() {
 
   return (
-    <CartContextProvider>
-      <Cart />
-      <Header/>
-      <Meals />
-    </CartContextProvider>
+    <>
+      HELLO
+    </>
   );
 }
 
